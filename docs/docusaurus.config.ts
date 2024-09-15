9f8eb85eef906c0bf5ac15e6c531a40824af1284
Andreas Sundström <andreas-sundstrom@hotmail.com>
2024-09-15 19:16:48 +0200

    docs: Add workflow

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Postgres administration',
+  tagline: 'Learn with examples',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
